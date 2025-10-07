55e2d5b9a38b4f477d57489ab6f49bea2c5b7f97
bigwaveBigwave <bluebiird909@naver.com>
2025-10-08 00:29:29 +0900

    현재값 이전값 바꾸기 로직 - 틀림

---
+++ CodingTest_C/10813.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N, M;
+	int u, k;
+	int temp = 0;
+
+	cin >> N;
+	cin >> M;
+
+	vector<int> v1(N);
+	for (int i = 0; i < N; i++) {
+		v1[i] = i;
+	}
+
+	for (int i = 0; i < M; i++) {
+		cin >> u;
+		cin >> k;
+
+		temp = v1[u - 1];
+		v1[u - 1] = v1[k - 1];
+		v1[k - 1] = v1[temp];
+
+	}
+
+	for (int i = 0; i < N; i++) {
+		cout << v1[i] << " ";
+	}
+
+
+
+
+
+	return 0;
+}
