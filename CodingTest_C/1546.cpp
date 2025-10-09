13bc3bcb416517facf2ba0904aeee0eeaff262b1
bigwaveBigwave <bluebiird909@naver.com>
2025-10-10 02:47:09 +0900

    스스로 풀기

---
+++ CodingTest_C/1546.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N;
+	cin >> N;
+	vector<int> v(N);
+	for (int i = 0; i < N; i++) {
+
+	}
+
+
+
+
+
+	return 0;
+}
