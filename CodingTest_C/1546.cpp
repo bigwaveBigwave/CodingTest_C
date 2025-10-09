b32f676b854b47d6e9fbb91e5db35149ee1d8904
bigwaveBigwave <bluebiird909@naver.com>
2025-10-10 02:53:57 +0900

    1. max 변수명, 초기화 2. 소수점 절사 문제 -> double 타입으로 변경 3. temp변수에 계속 더하는 로직 문제 4. 불필요한 조건문 -> 최대값도 100으로 포함돼야함.

--- CodingTest_C/1546.cpp
+++ CodingTest_C/1546.cpp
@@ -8,12 +8,31 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int N;
+	double N;
 	cin >> N;
+	double max_value = 0;
 	vector<int> v(N);
+	vector<int> v2(N);
 	for (int i = 0; i < N; i++) {
-
+		cin >> v[i];
+		if (v[i] > max_value) {
+			max_value = v[i];
+		}
 	}
+
+	double temp = 0;
+
+	for (int i = 0; i < N; i++) {
+		
+
+		v2[i] = v[i]/max_value*100;
+		temp = temp + v2[i];
+	}
+
+	cout << temp / N;
+
+
+
 
 
 
