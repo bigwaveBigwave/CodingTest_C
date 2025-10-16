bc0924a94f92947cab2e7f1c3bc8f1aea89db2d2
bigwaveBigwave <bluebiird909@naver.com>
2025-10-16 23:59:19 +0900

    기본 세팅

---
+++ CodingTest_C/2908.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	 
+
+
+
+
+
+	return 0;
+}
