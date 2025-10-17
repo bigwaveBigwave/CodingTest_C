8160c36ed32e6b2f4633c77eafd6832178b2a212
bigwaveBigwave <bluebiird909@naver.com>
2025-10-18 02:36:00 +0900

    문자열의 순서 바꾸기, int로 형 변환(stoi()), 문자열의 문자를 배열로 접근하기

--- CodingTest_C/2908.cpp
+++ CodingTest_C/2908.cpp
@@ -10,17 +10,54 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	 /*
-	 1. 문제 해석
-	 
-	 
-	 2. 손으로 풀기
-	 
-	 
-	 
-	 3. psuedo coding
-	 
-	 */
+	/*
+	1. 문제 해석
+	세자리 수가 두개 주어지면(0이 포함안된) 각 수를 뒤에서부터 읽어서 그 중 큰 수를구하기
+
+	2. 손으로 풀기
+	두 수를 문자열로 변수 두개로 입력 받기
+
+	각 수의 순서 거꾸로 하기
+
+
+	최댓값 구하기
+
+
+	3. psuedo coding
+	최댓값, 변수1, 역1, 변수2, 역2 선언
+	문자열로 입력받기. 2개 변수.
+	for(3번) {
+	string1_r[i] = string1[2-i]
+	string2_r[i] = string2[2-i]
+	}
+
+	if(int(string1_r) > int(string2_r)
+	cout << 1
+	}
+	else {
+	cout << 2
+	*/
+
+	int max = -1;
+	string s1, s1_r, s2, s2_r;
+
+	cin >> s1;
+	s1_r = s1;
+
+	cin >> s2;
+	s2_r = s2;
+
+	for (int i = 0; i < 3; i++) {
+		s1_r[i] = s1[2 - i];
+		s2_r[i] = s2[2 - i];
+	}
+
+	if (stoi(s1_r) > stoi(s2_r)) {
+		cout << s1_r;
+	}
+	else {
+		cout << s2_r;
+	}
 
 
 
