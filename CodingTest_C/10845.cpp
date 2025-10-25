967547c7b79c1ef58856b64388389505a370dbd8
bigwaveBigwave <bluebiird909@naver.com>
2025-10-25 23:57:30 +0900

    큐 명령어

---
+++ CodingTest_C/10845.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+#include<vector>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N;
+	cin >> N;
+	string command;
+	queue<int> q;
+
+	while (N--) {
+		cin >> command;
+		if (command == "push") {
+			int K;
+			cin >> K;
+			q.push(K);
+
+		}
+		else if (command == "pop") {
+			if (q.empty() != 1) {
+				q.front();
+				q.pop();
+				
+			}
+			cout << "비어있음" << " ";
+		}
+		else if (command == "size") {
+			cout << q.size() << " ";
+		}
+		else if (command == "empty") {
+			if (q.empty() != 1) {
+				cout << "0"<< " ";
+
+			}
+			cout << "1" << " ";
+		}
+		else if (command == "front") {
+			if (q.empty() != 1) {
+				cout << q.front() << " ";
+
+			}
+			cout << "-1" << " ";
+		}
+		else if (command == "back") {
+			if (q.empty() != 1) {
+				cout << q.back() << " ";
+
+			}
+			cout << "-1" << " ";
+		}
+
+	}
+
+	
+
+
+
+
+
+	return 0;
+}
