eaa2713c85b00f0ae205ac19406d85c94dbba20f
bigwaveBigwave <bluebiird909@naver.com>
2025-11-08 02:59:50 +0900

    while문으로 바꾸기, 거리배열의 값이 0이 아닐떄로 바꾸기(1이상 자연수로 저장돼있으니까)

--- CodingTest_C/pr_게임맵최단거리.cpp
+++ CodingTest_C/pr_게임맵최단거리.cpp
@@ -20,12 +20,11 @@ using namespace std;
 인덱스 에 0이 있거나 이미 방문했으면 다른방향
 해가지고 적절한 방향을 스택에 넣는다
 */
-
-int solution(vector<vector<int> > maps)
+int solution(vector<vector<int>> maps)
 {
     int n = maps.size();
     int m = maps[0].size();
-    vector<vector<int>> dist(n, vector<int>(m,0));
+    vector<vector<int>> dist(n, vector<int>(m, 0));
     int answer = 0;
     queue<pair<int, int>> s1;
     int dx[4] = { -1, 0, 1, 0 };
@@ -37,19 +36,19 @@ int solution(vector<vector<int> > maps)
 
     dist[0][0] = 1;
     s1.push({ 0,0 });
-    if (!s1.empty()) {
+    while (!s1.empty()) {
         pair<int, int> p = s1.front(); s1.pop();
 
         if (p.first == n - 1 && p.second == m - 1) return dist[p.first][p.second];
-        for (int i = 0; i < 4; i++){
+        for (int i = 0; i < 4; i++) {
             int nx = p.first + dx[i];
             int ny = p.second + dy[i];
             if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            if (maps[nx][ny] == 0 || dist[nx][ny] == 1) continue;
-            
+            if (maps[nx][ny] == 0 || dist[nx][ny] != 0) continue;
+
             dist[nx][ny] = dist[p.first][p.second] + 1;
             s1.push({ nx, ny });
-            }
+        }
 
     }
     //큐가 비었는데 도달 못했으면 -1
