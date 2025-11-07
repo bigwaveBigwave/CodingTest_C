3c725fe33894e933ffa410f15f21582f30004815
bigwaveBigwave <bluebiird909@naver.com>
2025-11-08 02:51:50 +0900

    .

--- CodingTest_C/pr_게임맵최단거리.cpp
+++ CodingTest_C/pr_게임맵최단거리.cpp
@@ -23,28 +23,35 @@ using namespace std;
 
 int solution(vector<vector<int> > maps)
 {
-
-    vector<vector<int>> v = maps;
+    int n = maps.size();
+    int m = maps[0].size();
+    vector<vector<int>> dist(n, vector<int>(m,0));
     int answer = 0;
     queue<pair<int, int>> s1;
-    int dx[] = { -1, 0, 1, 0 };
-    int dy[] = { 0, 1, 0, -1 };
+    int dx[4] = { -1, 0, 1, 0 };
+    int dy[4] = { 0, 1, 0, -1 };
 
+    //시작점이 벽이면 바로 -1
+    if (maps[0][0] == 0) return -1;
+
+
+    dist[0][0] = 1;
     s1.push({ 0,0 });
     if (!s1.empty()) {
         pair<int, int> p = s1.front(); s1.pop();
+
+        if (p.first == n - 1 && p.second == m - 1) return dist[p.first][p.second];
         for (int i = 0; i < 4; i++){
             int nx = p.first + dx[i];
             int ny = p.second + dy[i];
-            if (nx < 0 || nx >= maps.size()-1 || ny < 0 || ny >= maps[0].size()-1) continue;
-            if (maps[nx][ny] == 0 || v[nx][ny] == 1) continue;
-            if (nx == maps.size() && ny = maps[0].size()) return;
-            s.push({nx, ny});
-            s.pop();
-            v[nx][ny] = 1;
-            answer++;
-            }))
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if (maps[nx][ny] == 0 || dist[nx][ny] == 1) continue;
+            
+            dist[nx][ny] = dist[p.first][p.second] + 1;
+            s1.push({ nx, ny });
+            }
 
     }
-    return answer;
+    //큐가 비었는데 도달 못했으면 -1
+    return -1;
 }
