1e0e64ba81cae9e4fdcdaf5cfe8d69db6755d369
bigwaveBigwave <bluebiird909@naver.com>
2025-11-10 04:53:38 +0900

    부족한 부분: 거리배열 사용, 일수와 거리배열을 동일시, 일수가 동일하게 증가하려면 익은 토마토들을 먼저 큐에 넣고 Flood Fill

--- CodingTest_C/7569.cpp
+++ CodingTest_C/7569.cpp
@@ -3,82 +3,66 @@
 #include<algorithm>
 #include<tuple>
 using namespace std;
-#define X first
-#define Y second
-#define Z third
-//1의 상하좌우 위 아래의 0들을 1로 바꾸고 모두 1이 되는 날을 출력하라
-int d = 0; // 걸리는 일 수
+int dx[] = { -1, 0, 1, 0, 0, 0 };
+int dy[] = { 0, 1, 0, -1, 0, 0 };
+int dz[] = { 0, 0, 0, 0, 1, -1 };
+int board[200][200][200];
+int dist[200][200][200];
+queue<tuple<int, int, int>> q;
+int n, m, h;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int n, m, h;
-	cin >> n >> m >> h;
-
-	int board[200][200][200];
-	int vis[200][200][200];
-	queue<tuple<int, int, int>> q;
-
-	int dx[] = { -1, 0, 1, 0, 0, 0 };
-	int dy[] = { 0, 1, 0, -1, 0, 0 };
-	int dz[] = { 0, 0, 0, 0, 1, -1 };
-
+	
+	cin >> m >> n >> h;
+	//익은 토마토 방문하기
 	for (int k = 0; k < h; k++) {
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				cin >> board[k][i][j];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				int tmp;
+				cin >> tmp;
+				board[i][j][k] = tmp;
+				if (tmp == 1) q.push({ i, j, k });//익은 토마토
+				if (tmp == 0) dist[i][j][k] = -1;//안 익은 토마토
 			}
 		}
 	}
-	//1인 곳만 방문하는 과정
+	
+	//익은 토마토에서 인접한 안 익은 토마토를 익히고 거리배열에 +1 해서 일수 표현하기
+	while (!q.empty()) {
+		tuple<int, int, int> cur = q.front(); q.pop();
+
+		for (int dir = 0; dir < 6; dir++) {
+			int nx = get<0>(cur) + dx[dir];
+			int ny = get<1>(cur) + dy[dir];
+			int nz = get<2>(cur) + dz[dir];
+			if (nx < 0 || nx >= m || ny < 0 || ny >= n || nz < 0 || nz >= h) continue;
+			if (dist[nx][ny][nz] >= 0) continue;//아직 익지 않은 토마토면 현재 방문하고있는 위치 + 1
+			dist[nx][ny][nz] = dist[get<0>(cur)][get<1>(cur)][get<2>(cur)] + 1;
+			q.push({ nx, ny, nz });
+		}
+
+	}
+
+
+	//최소일수 구하기
+	int ans = 0;
 	for (int k = 0; k < h; k++) {
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if (vis[k][i][j] == 1) {
-					continue;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (dist[k][i][j] == -1) {
+					cout << -1;
+					return 0;
 				}
-				if (board[k][i][j] == 1) {
-					vis[k][i][j] = 1;
-
-
-					q.push({ k, i, j });
-					while (!q.empty()) {
-						tuple<int, int, int> cur = q.front(); q.pop();
-						d++;
-						//인접한 여섯방향이 범위안에 맞고 방문 안 했고 -1이 아니면 (1이면 방문, 0이면 인접방향을 모두 1로 바꾼 후 일수를 +1하고 방문)
-						for (int dir = 0; dir < 6; dir++) {
-							int nx = get<1>(cur) + dx[dir];
-							int ny = get<2>(cur) + dy[dir];
-							int nz = get<0>(cur) + dz[dir];
-							if (nx < 0 || nx >= m || ny < 0 || ny >= n || nz < 0 || nz >= h) continue;
-							if (vis[nz][nx][ny] == 1 || board[nz][nx][ny] < 0) continue;
-							if (board[nz][nx][ny] == 0) {
-								board[nz][nx][ny] = 1;
-								vis[nz][nx][ny] = 1;
-								q.push({ nz, nx, ny });
-							}
-							vis[nz][nx][ny] = 1;
-							q.push({ nz, nx, ny });
-						}
-					}
-				}
-				
+				ans = max(ans, dist[i][j][k]);
 				
 			}
 		}
 	}
-	for (int k = 0; k < h; k++) {
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if (board[k][i][j] == 1) {
-					cout << d;
-				}
-				cout << -1;
-				
-			}
-		}
-	}
+
+	cout << ans;
 
 	return 0;
 }
