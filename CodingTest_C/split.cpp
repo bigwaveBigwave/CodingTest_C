241188bee5419dbfc18674f4279683d139d64c8a
bigwaveBigwave <bluebiird909@naver.com>
2025-11-20 04:14:26 +0900

    deque

--- CodingTest_C/split.cpp
+++ CodingTest_C/split.cpp
@@ -5,6 +5,18 @@
 
 using namespace std;
 
+vector<string> split(string& s, string& sep) {
+	vector<string> ret;
+	int pos = 0;
+	while (pos < s.size()) {
+		int nxt_pos = s.find(sep, pos);
+		if (nxt_pos == -1) nxt_pos = s.size();
+		if (nxt_pos - pos > 0)
+			ret.push_back(s.substr(pos, nxt_pos - pos));
+		pos = nxt_pos + sep.size();
+	}
+}
+
 int main() {
 	string s1 = "welcome to the black parade";
 	string sep1 = " ";
