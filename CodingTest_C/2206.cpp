f3d530ca1bcf3017579690f862ea8b332279e671
bigwaveBigwave <bluebiird909@naver.com>
2025-11-24 23:35:10 +0900

    getline 제거

---
+++ CodingTest_C/2206.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<queue>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+int dx[] = { -1, 0, 1, 0 };
+int dy[] = { 0, 1, 0, -1 };
+//주어진 행렬의 0,0에서부터시작해서  N,M으로 가는 최단 경로를 출력하기
+//조건 : 0인 곳만 갈 수 있음. 필요시 한번은 벽을 부술수있음. 시작칸과 끝나는칸도 개수 포함
+int main() {
+    int N, M;
+    int answer = 0;//최단경로거리
+    cin >> N >> M;
+    vector<vector<int>> board(N, vector<int> (M));
+    vector<vector<int>> visit(N, vector<int> (M));//방문배열
+    queue<pair<int, int>> q;
+    for (int i = 0; i < N; i++) {
+        string a;
+        cin >> a;//getline은 줄 전체를 문자열로 읽음(cin은 단어 단위)
+        for (int j = 0; j < M; j++) {
+            board[i][j] = a[j];
+        }
+
+    }
+    visit[0][0] = 1;
+    answer = 1;
+    q.push({ 0, 0 });
+    while (!q.empty()) {
+        pair<int, int> cur = q.front(); q.pop();
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = cur.first + dx[dir];
+            int ny = cur.second + dy[dir];
+            if (nx < 0 || nx > N || ny < 0 || ny > M) continue;
+            if (visit[nx][ny] == 1 || board[nx][ny] == 1) continue;
+            visit[nx][ny] = 1;
+            q.push({ nx, ny });
+            answer += 1;
+        }
+
+    }
+    return 0;
+}
