cb843a8e6669b3c0eebd99668ece3aadcefc7ed1
bigwaveBigwave <bluebiird909@naver.com>
2025-11-26 03:25:50 +0900

    문제1.  변수 입력 전 변수 크기 배열 초기화 금지 (크기 0됨), 선언만 하고 나중에 resize() 사용

--- CodingTest_C/2206.cpp
+++ CodingTest_C/2206.cpp
@@ -8,43 +8,70 @@ int dx[] = { -1, 0, 1, 0 };
 int dy[] = { 0, 1, 0, -1 };
 //주어진 행렬의 0,0에서부터시작해서  N,M으로 가는 최단 경로를 출력하기
 //조건 : 0인 곳만 갈 수 있음. 필요시 한번은 벽을 부술수있음. 시작칸과 끝나는칸도 개수 포함
-int main() {
-    int N, M;
-    int answer = 0;//최단경로거리
-    cin >> N >> M;
-    vector<vector<int>> board(N, vector<int> (M));
-    //vector<vector<int>> dist(N, vector<int> (M));//최단거리배열
 
-    //dist[x][y][b] : (x, y)에 벽을 b번 부순 상태(0 또는 1)로 도착한 최단 거리
-    vector<vector<vector<int>>> dist(N, vector<vector<int>> (M, vector<int>(2, 0)));//최단거리배열
-    queue<pair<int, int>> q;
-    for (int i = 0; i < N; i++) {
-        string a;
-        cin >> a;//getline은 줄 전체를 문자열로 읽음(cin은 단어 단위)
-        for (int j = 0; j < M; j++) {
-            board[i][j] = a[j] - '0';//char타입이니까 정수형으로 변환
+int n, m;
+
+vector<vector<int>> board;
+//vector<vector<int>> dist(N, vector<int> (M));//최단거리배열
+
+//dist[x][y][b] : (x, y)에 벽을 b번 부순 상태(0 또는 1)로 도착한 최단 거리
+vector<vector<vector<int>>> dist;//최단거리배열
+
+
+//맵 밖으로 나가면 true를 반환하는 함수
+bool OOB(int x, int y) {
+    return x < 0 || x >= n || y < 0 || y >= m;
+}
+
+int bfs() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            dist[i][j][0] = dist[i][j][1] = -1;
+            //모든 거리를 -1로 초기화, "-1은 아직 한번도 방문 안 함"을 의미
         }
-
     }
-    dist[0][0] = 1;
-    answer = 1;
-    q.push({ 0, 0 });
+    dist[0][0][0] = dist[0][0][1] = 1;
+    queue<tuple<int, int, int>> q;
+    q.push({ 0, 0, 0 });
     while (!q.empty()) {
-        pair<int, int> cur = q.front(); q.pop();
+        int x, y, broken;
+        tie(x, y, broken) = q.front();//tie를 써서 세 변수로 분해해서 받기
+        if (x == n - 1 && y = m - 1) return dist[x][y][broken];
+        q.pop();
+        int nextdist = dist[x][y][broken] + 1;
         for (int dir = 0; dir < 4; dir++) {
-            int nx = cur.first + dx[dir];
-            int ny = cur.second + dy[dir];
-            if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-            if (dist[nx][ny] != 0 || board[nx][ny] == 1) continue;
-            dist[nx][ny] = dist[cur.first][cur.second] + 1;
-            q.push({ nx, ny });
-            if (nx == N - 1 && ny == M - 1) {
-                cout << dist[nx][ny] << "\n";
-                return;//break, return 차이
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if (OOB(nx, ny)) continue;
+            if (dist[nx][ny][broken] == -1 && board[nx][ny] == 0) {
+                dist[nx][ny][broken] = nextdist;
+                q.push({ nx, ny, broken });
+            }
+
+            if (!broken && board[nx][ny] == 1 && dist[nx][ny][1] == -1) {
+                dist[nx][ny][1] = nextdist;
+                q.push({ nx, ny, 1 });
             }
         }
 
     }
+    return -1;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n >> m;
+    board.resize(n, vector<int>(m));
+    dist.resize(n, vector<vector<int>>(m, vector<int>(2, 0)));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> board[i][j];
+        }
+    }
+    cout << bfs();
+
 
     return 0;
 }
