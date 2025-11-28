6ba6611e0a7d2471f524e84b9abfaff0e481a743
bigwaveBigwave <bluebiird909@naver.com>
2025-11-29 02:59:16 +0900

    문제 입력 그대로 입력받기(y가행, x가열인부분은 충족) 넓이는 좌표값의 곱이 아닌 직사각형의 개수 -> bfs돌 때 1씩 증가(bfs 새로 돌때 1로 초기화)

--- CodingTest_C/영역구하기.cpp
+++ CodingTest_C/영역구하기.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 int dx[] = { -1, 0, 1, 0 };
 int dy[] = { 0, 1, 0, -1 };
-int e1, e2 = 0;
+
 
 int main() {
     ios::sync_with_stdio(false);
@@ -15,29 +15,34 @@ int main() {
     vector<int> extent;
     int m, n, k;
     cin >> m >> n >> k;
-    vector<vector<int>> board(m, vector<int>(n));
-    vector<vector<int>> dist(m, vector<int>(n));
+    vector<vector<int>> board(m, vector<int>(n, 0));
+    vector<vector<int>> dist(m, vector<int>(n, 0));
     queue<pair<int, int>> q;//pair 문법
     //board를 1 또는 0 으로 채우기
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; i < 2; j++) {
-            int x1, y1, x2, y2;
-            cin >> x1 >> y1 >> x2 >> y2;
-            for (int k = x1; k < x2; k++) {
-                for (int l = y1; 1 < y2; l++) {
-                    board[k][l] = 1;
+    for (int i = 0; i < k; i++) {
+        
+            int y1, x1, y2, x2;
+            cin >> x1 >> y1 >> x2 >> y2; // 입력은 문제 그대로!
+            for (int w = y1; w < y2; w++) {
+                for (int l = x1; l < x2; l++) {
+                    board[w][l] = 1;
                 }
             }
-        }
+        
     }
+
+    
     //BFS
     for (int p = 0; p < m; p++) {
         for (int r = 0; r < n; r++) {
-            if (board[p][r] != 1) {
+            if (board[p][r] != 1 && dist[p][r] == 0) {//bfs 시작 조건 추가(방문안했고 벽아닌곳)
                 dist[p][r] = 1;
+                int width = 1;
                 q.push({ p, r });
+                count++;
                 while (!q.empty()) {
                     pair<int, int> cur = q.front(); q.pop();
+                    
                     for (int dir = 0; dir < 4; dir++) {
                         int nx = cur.first + dx[dir];
                         int ny = cur.second + dy[dir];
@@ -45,19 +50,19 @@ int main() {
                         if (dist[nx][ny] > 0 || board[nx][ny] == 1) continue;
                         dist[nx][ny] = (nx + 1) * (ny + 1);
                         q.push({ nx, ny });
-                        e1 = nx;
-                        e2 = ny;
+                        width++;
                     }
                 }
-                count += 1;
-                extent.push_back((e1 + 1) * (e2 + 1));
+                
+                //넓이 계산 잘못됨
+                extent.push_back(width);
             }
 
         }
     }
-    sort(extent.begin(), extent.end(), greater<>());//내림차순
+    sort(extent.begin(), extent.end());//오름차순
     cout << count << '\n';
-    for (int t = 0; t < count; t++) {
+    for (int t = 0; t < extent.size(); t++) {
         cout << extent[t] << " ";
     }
 
