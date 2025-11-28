a30eead6be37dfc8799068cd15abbf3aadf5b1ea
bigwaveBigwave <bluebiird909@naver.com>
2025-11-28 12:10:33 +0900

    bfs 시작 부분 반복문 추가, 넓이 계산 추가, 사각형 넓이 좌표 변환 추가

---
+++ CodingTest_C/영역구하기.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int dx[] = { -1, 0, 1, 0 };
+int dy[] = { 0, 1, 0, -1 };
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int m, n, k;
+    cin >> m >> n >> k;
+    vector<vector<int>> board;
+    vector<vector<int>> dist;
+    int answer = 0;
+    queue < pair{int, int} > q;
+    return 0;
+}
