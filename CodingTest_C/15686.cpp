044789836850e04d94d994176b970505f48e81f0
bigwaveBigwave <bluebiird909@naver.com>
2025-12-08 00:05:04 +0900

    nextPermutation 사용

--- CodingTest_C/15686.cpp
+++ CodingTest_C/15686.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include<vector>
 #include<queue>
+#include<algorithm>
 using namespace std;
 
 #define X first
@@ -23,6 +24,20 @@ int main(void) {
         }
     }
     vector<int> brute(chicken.size(), 1);
-    fill(brute.begin(), brute.begin() + chicken.size() - m, 0);)
+    fill(brute.begin(), brute.begin() + chicken.size() - m, 0);//앞의 chicken.size() - m 칸은 0, 뒤의 m 칸은 1
+    int mn = 0x7f7f7f7f;//답을 저장할 변수
+    do {
+        int dist = 0;//도시의 치킨 거리를 저장할 변수
+        for (auto h : house) {
+            int tmp = 0x7f7f7f7f;//집의 치킨 거리를 저장할 변수
+            for (int i = 0; i < chicken.size(); i++) {
+                if (brute[i] == 0) continue;
+                tmp = min(tmp, abs(chicken[i].X - h.X) + abs(chicken[i].Y - h.Y));//집의 치킨 거리 갱신
+            }
+            dist += tmp;
+        }
+        mn = min(mn, dist);
+    } while (next_permutation(brute.begin(), brute.end()));
+    cout << mn;
     return 0;
 }
