1121eaa769320472aac04f43dffbec9cfb84167d
bigwaveBigwave <bluebiird909@naver.com>
2025-12-07 23:59:30 +0900

    브루트

--- CodingTest_C/15686.cpp
+++ CodingTest_C/15686.cpp
@@ -7,10 +7,22 @@ using namespace std;
 #define Y second
 
 int board[55][55];
-
+int n, m;
+vector<pair<int, int>> chicken;
+vector<pair<int, int>> house;
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> board[i][j];
+            if (board[i][j] == 1) house.push_back({ i, j });
+            if (board[i][j] == 2) house.push_back({ i, j });
+        }
+    }
+    vector<int> brute(chicken.size(), 1);
+    fill(brute.begin(), brute.begin() + chicken.size() - m, 0);)
     return 0;
 }
