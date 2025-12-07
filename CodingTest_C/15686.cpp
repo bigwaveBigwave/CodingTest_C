311f282902e7d1753dfb3e5b5a5219a875a83e86
bigwaveBigwave <bluebiird909@naver.com>
2025-12-07 23:55:53 +0900

    기본 세팅

---
+++ CodingTest_C/15686.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+#define X first
+#define Y second
+
+int board[55][55];
+
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    return 0;
+}
