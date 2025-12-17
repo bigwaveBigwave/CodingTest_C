13af97760b86095d5f9a595e362cb4028d541bd2
bigwaveBigwave <bluebiird909@naver.com>
2025-12-17 20:59:34 +0900

    배열 방문 조건 : n-1까지 거리 배열 : bfs시작 부분은 0 최댓값 : 큐 빌 때마다 최댓값 비교(max answer, cnt -> answer)

--- CodingTest_C/bfs연습.cpp
+++ CodingTest_C/bfs연습.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<queue>
+#include<vector>
 using namespace std;
 int dx[] = { -1, 0, 1, 0 };
 int dy[] = { 0, 1, 0, -1 };
@@ -9,50 +10,46 @@ int main() {
 	int N, M;
 	cin >> N >> M;
 	vector<vector<int>> board(N, vector<int>(M));
-	vector<vector<int>> dist(N, vector<int>(M));
+	vector<vector<int>> dist(N, vector<int>(M, -1));
 	queue<pair<int, int>> q;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
 			cin >> board[i][j];
 		}
 	}
+	int answer = 0;
 	vector<int> maxValue;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			if (board[i][j] == 1) {
-				dist[i][j] = 1;
+			if (board[i][j] == 1 && dist[i][j] == -1) {//방문 안 한 곳만 bfs
+				dist[i][j] = 0;//시작점 거리
 				q.push({ i, j });
-				int maxV = 0;
+				int cnt = 1;//섬크기
 				while (!q.empty()) {
 					pair<int, int> curr = q.front(); q.pop();
 					for (int dir = 0; dir < 4; dir++) {
 						int nx = curr.first + dx[dir];
 						int ny = curr.second + dy[dir];
-						if (nx < 0 || nx > N || ny < 0 || ny > M) {
+						if (nx < 0 || nx >= N || ny < 0 || ny >= M) {//N-1, M-1까지만 배열임
 							continue;
 						}
-						if (dist[nx][ny] != 0 || board[nx][ny] == 0) {
+						if (dist[nx][ny] != -1 || board[nx][ny] == 0) {
 							continue;
 						}
 						dist[nx][ny] = dist[curr.first][curr.second] + 1;
 						q.push({ nx, ny });
-						maxV++;
+						cnt++;
 					}
 				}
-				maxValue.push_back(maxV);
+				answer = max(answer, cnt);
 
 			}
 			
 		}
 	}
 
-	int answer = 0;
-	for (int v = 0; v < maxValue.size(); v++) {
-		if (maxValue[v] > answer) {
-			answer = maxValue[v];
-		}
-
-	}
+	
+	
 	cout << answer;
 
 	return 0;
