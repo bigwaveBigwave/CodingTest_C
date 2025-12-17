ccdf31302a34c5b02de670221d40074f2681fab9
bigwaveBigwave <bluebiird909@naver.com>
2025-12-18 00:30:07 +0900

    벡터 크기 할당

--- CodingTest_C/14888.cpp
+++ CodingTest_C/14888.cpp
@@ -29,6 +29,22 @@ void dfs(int idx, long long curr) {
     if (op[0] > 0) {
         op[0]--;
         dfs(idx + 1, curr + A[idx]);
+        op[0]++;
+    }
+    if (op[1] > 0) {
+        op[1]--;
+        dfs(idx + 1, curr - A[idx]);
+        op[1]++;
+    }
+    if (op[2] > 0) {
+        op[2]--;
+        dfs(idx + 1, curr * A[idx]);
+        op[2]++;
+    }
+    if (op[3] > 0) {
+        op[3]--;
+        dfs(idx + 1, curr / A[idx]);
+        op[3]++;
     }
 }
 
@@ -37,16 +53,16 @@ int main() {
     cin.tie(nullptr);
     
     cin >> N;
-    vector<int> v(N);
+    A.resize(N);
     for (int i = 0; i < N; i++) {
-        cin >> v[i];
+        cin >> A[i];
     }
     
     for (int j = 0; j < 4; j++) {
         cin >> op[j];
     }
     
-    dfs(1, A[0]);
+    dfs(1, A[0]);//dfs(idx, curr) : idx번쨰 숫자를 다음에 사용할 차례고 curr은 그 전까지 계산한 결과
 
     cout << mx << "\n" << mn << "\n";
     return 0;
