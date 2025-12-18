1b794f8d2270ff0a52d06ac1400fb0c8b028cf18
bigwaveBigwave <bluebiird909@naver.com>
2025-12-18 15:22:47 +0900

    dfs + 백트래킹

--- CodingTest_C/14500.cpp
+++ CodingTest_C/14500.cpp
@@ -21,39 +21,87 @@ using namespace std;
 1 - 3) 각 최댓값의 위치에서 BFS를 돌리면 값이 계속 나올텐데 그 중 최댓값을 계속 갱신하기.
 */
 
-//수도코드 2
-/*
-종이의 크기 N, M 입력받기
-종이 벡터 선언
 
-각 모양들을 백트래킹 -- 1)
-1 - 1) 각 모양을 회전시킨 경우의 수도 포함해서 백트래킹
-1 - 2) 가장 큰 수의 주변에서 BFS 세 번 돌리기
-1 - 3) 각 최댓값의 위치에서 BFS를 돌리면 값이 계속 나올텐데 그 중 최댓값을 계속 갱신하기.
-*/
 
-int dx[] = { -1, 0, 1, 0 };
-int dy[] = { 0, 1, -1, 0 };
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
 int N, M;
 int maxValue = 0;//숫자판의 최댓값
 int answer = 0;//목표인 최댓값
+vector<vector<int>> board;
+vector<vector<bool>> visit;
+
+void dfs(int x, int y, int depth, int sum) {
+    if (sum + (4 - depth) * maxValue <= answer) return;
+
+    if (depth == 4) {
+        answer = max(answer, sum);
+        return;
+    }
+    for (int dir = 0; dir < 4; dir++) {
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+        if (visit[nx][ny] == 1) {
+            continue;
+        }
+
+        visit[nx][ny] = true;
+        dfs(nx, ny, depth + 1, sum + board[nx][ny]);
+        visit[nx][ny] = false;
+    }
+}
+
+void checkT(int x, int y) {
+    int center = board[x][y];
+    int neighborSum = 0;
+    int cnt = 0;
+    int mn = INT_MAX;
+
+    for (int dir = 0; dir < 4; dir++) {
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+
+        if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+
+        cnt++;
+        neighborSum += board[nx][ny];
+        mn = min(mn, board[nx][ny]);
+    }
+
+    if (cnt >= 3) {
+        if (cnt == 4) {
+            answer = max(answer, center + (neighborSum - mn));
+
+        }
+        else
+            answer = max(answer, center + neighborSum);
+    }
+
+}
 int main() {
     cin >> N >> M;
-    vector<vector<int>> board(N, vector<int>(M));
-    //vector<vector<int>> visit(N, vector<int> (M, 0));
+    board.resize(N, vector<int>(M));
+    visit.resize(N, vector<bool>(M, false));
     //vector<pair<int, int>> maxValueSearch;
     //queue<pair<int, int>> q;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             cin >> board[i][j];
+            maxValue = max(maxValue, board[i][j]);
         }
     }
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            cin >> board[i][j];
+            visit[i][j] = true;
+            dfs(i, j, 1, board[i][j]);//시작 칸 포함 -> depth = 1
+            visit[i][j] = false;
+
+            checkT(i, j);
         }
     }
+    cout << answer << "\n";
 
     return 0;
 }
