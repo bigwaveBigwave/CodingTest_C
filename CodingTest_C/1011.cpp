4ed9dc6ab1c09ec97f53e264bffa9f0315ae75d5
bigwaveBigwave <bluebiird909@naver.com>
2026-01-03 03:32:42 +0900

    .

--- CodingTest_C/1011.cpp
+++ CodingTest_C/1011.cpp
@@ -6,21 +6,21 @@ int dist = 0;//이동 거리
 
 int solution(int x, int y) {
 
-    if (y - x == 1) {
-        answer += 1;
-        return answer;
-    }
-    else {
+    long long d = y - x;
+    long long m = (long long)sqrt((long double)d);
 
-    }
+    if (m * m == d) return (int)(2 * m - 1);
+    if (d <= m * m + m) return (int)(2 * m);
+    return (int)(2 * m + 1);
 }
 int main() {
-    int T, x, y;
+    int T;
+    long long x, y;
     cin >> T;
 
     while (T--) {
         cin >> x >> y;
-        solution(x, y);
+        cout << solution(x, y) << '\n';
     }
     return 0;
 }
