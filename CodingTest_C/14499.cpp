654a6c9d9522676582d90d2c1ad8a047bb52c1eb
bigwaveBigwave <bluebiird909@naver.com>
2026-01-05 03:05:51 +0900

    처음

--- CodingTest_C/14499.cpp
+++ CodingTest_C/14499.cpp
@@ -1,88 +1,102 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
 using namespace std;
+//문제 해석 : 주사위의 위치 좌표와 이동 명령을 보고 주사위 이동마다 주사위 상단의 값을 도출하기.
+//지도의 바깥으로 주사위를 이동시켜야할 때는 무시하기
+
+//수도코딩
+/*
+지도 행렬, 주사위 현재 위치 좌표, 명령 개수 선언, 입력받기
+주사위 상단 변수 선언(초기값 6)
+주사위 하단 변수 선언(초기값 1)
+위치 배열 선언
+x[4] = 0,0,-1,1
+y[4] = 1,-1,0,0
+지도 좌표값 입력받기
+while(명령개수--) {
+명령 변수 선언, 입력받기
+명령변수 입력받을때마다{
+해당 방향으로 주사위의 위치 좌표 이동. 위치 배열 이용 -> 근데 지도 좌표값의 범위를 조건으로달고 범위 안 넘을때만 이동
+주사위 위의 값 출력(방향의 반대에 있는 수가 위로 가게 됨) -> 명령 값이 1이면 상단값 4, 바닥값은 3
+
+좌표값이 0이면 주사위의 바닥면의 숫자가 좌표값으로 이동
+좌표값이 0이 아니면(=주사위 값만 존재) 칸의 수가 주사위 바닥으로 이동
+}
+
+
+}
+*/
+
+/*<구조화>
+1. 계속 변화되는 상태
+1 ) 주사위 6면 값 = 위 아래 북 남 서 동
+2 ) 현재 위치
+3 ) 지도 값
+
+2. 한 턴 로직
+1) 다음칸 계산
+2) 범위 체크
+3) 이동확정, 주사위 회전
+4) 지도칸과 주사위 바닥면 교환
+5) 주사위의 위 값 출력
+
+3. 핵심 : 주사위 회전 규칙을 구조화
+d[0] = top
+d[1] = bottom
+d[2] = north
+d[3] = south
+d[4] = west
+d[5] = east
+
+명령별 회전은 면의 자리바꿈
+동
+top <- west
+bottom <- east
+west <- bottom
+east <- top
+
+서
+top <- ease
+bottom <- west
+west <- top
+east <- bottom
+
+북
+남
+
+
+*/
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    int N, M, x, y, count;
+    cin >> N >> M >> x >> y >> count;
+    vector<vector<int>> v(N, vector<int>(M));
 
-    int N, M, x, y, K;
-    cin >> N >> M >> x >> y >> K;
 
-    vector<vector<int>> board(N, vector<int>(M));
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < M; j++)
-            cin >> board[i][j];
-
-    // dice indices:
-    // 0: top, 1: bottom, 2: north, 3: south, 4: west, 5: east
-    int dice[6] = { 0, 0, 0, 0, 0, 0 };
-
-    // direction: 1 east, 2 west, 3 north, 4 south
-    int dx[5] = { 0, 0, 0, -1, 1 };
-    int dy[5] = { 0, 1, -1, 0, 0 };
-
-    while (K--) {
-        int dir;
-        cin >> dir;
-
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
-
-        // out of bounds -> ignore
-        if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-
-        // roll dice
-        int top = dice[0], bottom = dice[1], north = dice[2], south = dice[3], west = dice[4], east = dice[5];
-
-        if (dir == 1) { // east
-            dice[0] = west;
-            dice[1] = east;
-            dice[4] = bottom;
-            dice[5] = top;
-            // north/south unchanged
-            dice[2] = north;
-            dice[3] = south;
+    //지도 좌표 값 입력 받기
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> v[i][j];
         }
-        else if (dir == 2) { // west
-            dice[0] = east;
-            dice[1] = west;
-            dice[4] = top;
-            dice[5] = bottom;
-            dice[2] = north;
-            dice[3] = south;
-        }
-        else if (dir == 3) { // north
-            dice[0] = south;
-            dice[1] = north;
-            dice[2] = top;
-            dice[3] = bottom;
-            dice[4] = west;
-            dice[5] = east;
-        }
-        else if (dir == 4) { // south
-            dice[0] = north;
-            dice[1] = south;
-            dice[2] = bottom;
-            dice[3] = top;
-            dice[4] = west;
-            dice[5] = east;
-        }
-
-        // move
-        x = nx; y = ny;
-
-        // copy between dice bottom and board cell
-        if (board[x][y] == 0) {
-            board[x][y] = dice[1];
-        }
-        else {
-            dice[1] = board[x][y];
-            board[x][y] = 0;
-        }
-
-        // print top
-        cout << dice[0] << "\n";
     }
 
+    while (count--) {
+        int instruction;
+        cin >> instruction;
+        x = x + dx[instruction];
+        y = y + dy[instruction];
+        if (x < 0 || y < 0 || x >= N || y >= M) {
+            continue;
+        }
+        
+            if (v[x][y] == 0) {
+                v[x][y] = down;
+            }
+            else {
+                down = v[x][y];
+                cube[1][1] = down;
+            }
+
+    }
     return 0;
 }
